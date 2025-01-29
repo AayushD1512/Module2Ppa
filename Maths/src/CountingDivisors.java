9d9e3c2808743a253366f28f62198386b2705e2b
AayushD1512 <aayushtechnical@gmail.com>
2025-01-29 11:18:18 +0530

    First

--- Maths/src/CountingDivisors.java
+++ Maths/src/CountingDivisors.java
@@ -1,139 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class CountingDivisors {
+public class CountingDivisors
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        int MAX = 1000000;
+        int[] spf = new int[MAX + 1]; // Smallest Prime Factor (SPF)
 
-    public static void countingDivisors(long n) {
-        int[] primes = new int[(int) (n + 1)];
-        int[] smallestPrimeFactors = new int[(int) (n + 1)];
-        int count = 0, ans = 1;
-
-        // Initialize primes and smallestPrimeFactors
-        for (int i = 0; i <= n; i++) {
-            primes[i] = 1; // Assume all numbers are prime initially
-            smallestPrimeFactors[i] = -1;
-        }
-
-        // Mark 0 and 1 as not prime
-        primes[0] = 0;
-        primes[1] = 0;
-
-        // Sieve of Eratosthenes to populate smallest prime factors
-        for (int i = 2; i * i <= n; i++) {
-            if (primes[i] == 1) {
-                for (int j = i; i * j <= n; j++) {
-                    primes[i * j] = 0; // Mark as non-prime
-                    smallestPrimeFactors[i * j] = i; // Record smallest prime factor
-                }
-            }
-        }
-
-        int initial = smallestPrimeFactors[(int) n];
-        while (smallestPrimeFactors[(int) n] != -1) {
-            if (initial == smallestPrimeFactors[(int) n]) {
-                count++;
-                ans *= (count + 1);
-            } else {
-                count = 1;
-                initial = smallestPrimeFactors[(int) n];
-            }
-            n /= smallestPrimeFactors[(int) n];
-            System.out.print(count + " ");
-        }
-
-        if (n != 1) {
-            count = 1;
-            System.out.print(count + " ");
-            ans *= (count + 1);
-        }
-        ans *= (count + 1);
-        System.out.println("ans: " + ans);
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // Read number of queries
-        int queries = sc.nextInt();
-        while (queries-- > 0) {
-            long n = sc.nextLong();
-            countingDivisors(n);
-        }
-
-        sc.close();
-    }
-}
-
-
-/*
-
-    import java.util.*;
-
-public class UniquePrimeFactors {
-
-    // Precompute smallest prime factors (SPF) for numbers up to MAX
-    private static final int MAX = 1000000;
-    private static int[] spf = new int[MAX + 1]; // Smallest Prime Factor array
-
-    // Function to compute the SPF array using a sieve-like approach
-    private static void computeSPF() {
+        // --> Following is also a way where you can calculate SPF without doing it side by side of Sieve calculation
+        // Initialize SPF using Sieve of Eratosthenes
         for (int i = 1; i <= MAX; i++) {
-            spf[i] = i; // Initialize each number to itself
+            spf[i] = i; // Initialize with self
         }
+
         for (int i = 2; i * i <= MAX; i++) {
-            if (spf[i] == i) { // If `i` is a prime number
+            if (spf[i] == i) { // i is prime
                 for (int j = i * i; j <= MAX; j += i) {
-                    if (spf[j] == j) {
-                        spf[j] = i; // Update SPF for multiples of `i`
-                    }
+                    if (spf[j] == j) spf[j] = i; // Set the smallest prime factor
                 }
             }
         }
-    }
 
-    // Function to count the number of unique prime factors of a number
-    private static int countUniquePrimeFactors(int num) {
-        int uniqueCount = 0;
-        int lastFactor = -1;
-
-        while (num != 1) {
-            int currentFactor = spf[num];
-            if (currentFactor != lastFactor) {
-                uniqueCount++; // Count a new unique prime factor
-                lastFactor = currentFactor;
-            }
-            num /= currentFactor; // Reduce the number
-        }
-
-        return uniqueCount;
-    }
-
-    public static void main(String[] args) {
+        // Input Processing
         Scanner sc = new Scanner(System.in);
-
-        // Precompute SPF array
-        computeSPF();
-
         int T = sc.nextInt(); // Number of test cases
-        StringBuilder result = new StringBuilder();
 
         while (T-- > 0) {
             int N = sc.nextInt(); // Number of elements in the array
             int[] A = new int[N];
-
             for (int i = 0; i < N; i++) {
                 A[i] = sc.nextInt();
             }
 
-            for (int i = 0; i < N; i++) {
-                result.append(countUniquePrimeFactors(A[i])).append(" ");
+            for (int num : A) {
+                int count = 0;
+                int lastPrime = -1; // To track unique primes
+                while (num > 1) {
+                    int prime = spf[num]; // Current smallest prime factor
+                    if (prime != lastPrime) { // Count only unique primes
+                        count++;
+                        lastPrime = prime;
+                    }
+                    num /= prime;
+                }
+                System.out.print(count + " "); // Print the count for this number
             }
-            result.append("\n");
+            System.out.println(); // Move to the next line after each test case
         }
-
-        System.out.print(result.toString());
-        sc.close();
     }
 }
-
-
- */
