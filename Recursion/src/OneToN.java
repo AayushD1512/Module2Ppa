dd6e04c57bb01b6944a687fb68970d06299d4290
AayushD1512 <aayushtechnical@gmail.com>
2025-02-17 17:34:38 +0530

    First

---
+++ Recursion/src/OneToN.java
@@ -0,0 +1,26 @@
+public class OneToN {
+    public static void main(String[] args) {
+        int n = 4;
+        int x = 1;
+        print(x,n); // 1 to N natural no.s printing.
+        print2(4);
+    }
+    //for printing 1 to N
+    static void print(int x, int n){
+        if(x>n) return;
+        System.out.print(x+" ");
+        x++;
+        print(x,n);
+    }
+
+    //for printing N to 1
+    static void print2(int n){
+        if(n<1){
+            return;
+        }
+        System.out.print(n+" ");
+        n--;
+        print2(n);
+
+    }
+}
